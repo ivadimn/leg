0b2b9778cbbbc23a3f2eb8b1603718cee4340e52
ivadimn <ivadimn@mail.ru>
2018-09-18 21:46:32 +0300

    custom toast

--- MyFirstApp/app/src/main/java/ru/ivadimn/myfirstapp/LaunchActivity.java
+++ MyFirstApp/app/src/main/java/ru/ivadimn/myfirstapp/LaunchActivity.java
@@ -4,11 +4,16 @@ import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LaunchActivity extends AppCompatActivity {
+
+    private final static String TAG = "......LAUNCH-ACTIVITY";
 
     private Button mBtnClick;
     private TextView mText;
@@ -17,6 +22,7 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        Log.d(TAG, "Into OnCreate M<ethod");
 
 
         final String newString = getString(R.string.new_text);
@@ -36,9 +42,22 @@ public class LaunchActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mText.setText(newString);
+                showCustomToast(newString);
             }
         });
 
 
+    }
+
+    public void showCustomToast(String msg) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.activity_toast, null);
+
+        TextView text = layout.findViewById(R.id.message_id);
+        text.setText(msg);
+
+        Toast toast = new Toast(this);
+        toast.setView(layout);
+        toast.show();
     }
 }
