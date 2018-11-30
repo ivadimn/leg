ef4366c76660c20f4319d3d9f7c53258e59c6465
vadim <ivadimn@mail.ru>
2018-11-30 16:15:08 +0300

    update

--- MThreading/src/MainClass.java
+++ MThreading/src/MainClass.java
@@ -10,30 +10,25 @@ public class MainClass {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(mThreads);
         Lock lock = new ReentrantLock();
-        new Thread(new Runnable() {
-            @Override
-            public void run()  {
-                for (int i = 0; i < mFloors; i++) {
-                    Future<Integer> future = executorService.submit(new CountCallable());
-                    //while(!future.isDone()) {
-                    //    TimeUnit.MILLISECONDS.sleep(100);
-                    //}
-                    try {
-                    //    lock.lock();
-                        mCount += future.get();
-                    }
-                    catch(Exception e) {
-                        e.printStackTrace();
-                    }
-                    //finally {
-                    //    lock.unlock();
-                    //}
-
-                }
-                System.out.println("Всего - " + mCount);
+        for (int i = 0; i < mFloors; i++) {
+            Future<Integer> future = executorService.submit(new CountCallable());
+            //while(!future.isDone()) {
+            //    TimeUnit.MILLISECONDS.sleep(100);
+            //}
+            try {
+                lock.lock();
+                mCount += future.get();
             }
-        }).start();
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                lock.unlock();
+            }
 
+        }
+        executorService.shutdown();
+        System.out.println("Всего - " + mCount);
     }
 
     public static class CountCallable implements Callable<Integer> {
@@ -43,6 +38,7 @@ public class MainClass {
             for (int i = 0; i < 100; i++) {
                 count++;
             }
+            System.out.println(Thread.currentThread().getName());
             return count;
         }
     }
