1712e9a1fde207010c02845986857cca8953d227
vadim <ivadimn@mail.ru>
2019-05-21 11:31:37 +0300

    update project

--- BarService/app/src/main/java/ru/ivadimn/barservice/MainActivity.java
+++ BarService/app/src/main/java/ru/ivadimn/barservice/MainActivity.java
@@ -1,7 +1,14 @@
 package ru.ivadimn.barservice;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
@@ -20,22 +27,28 @@ public class MainActivity extends AppCompatActivity {
 
     private ProgressBar pbProcess;
     private Button btnReduce;
-    private ScheduledExecutorService executorService;
-    private int progressValue = 0;
+    private boolean bound = false;
+    private ProcessService processService;
 
-    ProcessService.UpdateUI updateProgressBar = new ProcessService.UpdateUI() {
-        @Override
-        public void updateUI(int value) {
-            pbProcess.setProgress(value);
+    View.OnClickListener  onReduceClickListener = (view) ->  {
+        if(processService != null) {
+            processService.reduce();
         }
     };
 
-    Runnable processTask = () -> {
-        progressValue += 5;
-        if (progressValue > 100) {
-            progressValue = 0;
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            ProcessService.ProcessBinder processBinder
+                    = (ProcessService.ProcessBinder) iBinder;
+            processService = processBinder.getProcessService();
+            bound = true;
         }
-        pbProcess.setProgress(progressValue);
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            bound = false;
+        }
     };
 
     @Override
@@ -45,23 +58,39 @@ public class MainActivity extends AppCompatActivity {
 
         pbProcess = findViewById(R.id.pb_process);
         btnReduce = findViewById(R.id.btn_reduce);
-        executorService = new ScheduledThreadPoolExecutor(1);
+        btnReduce.setOnClickListener(onReduceClickListener);
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        executorService.scheduleAtFixedRate(processTask, 200, 200, TimeUnit.MILLISECONDS);
+        Intent intent = new Intent(this, ProcessService.class);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+        process();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        executorService.shutdownNow();
+        if (bound) {
+            unbindService(serviceConnection);
+            bound = false;
+        }
     }
 
     private void process() {
-
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int progress;
+                if (processService != null) {
+                    progress = processService.getProcessValue();
+                    pbProcess.setProgress(progress);
+                }
+                handler.postDelayed(this, 200);
+            }
+        });
     }
 }
--- BarService/app/src/main/java/ru/ivadimn/barservice/ProcessService.java
+++ BarService/app/src/main/java/ru/ivadimn/barservice/ProcessService.java
@@ -5,23 +5,33 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ProcessService extends Service {
 
+    public static final int MAX_VALUE = 100;
+    public static final int DELTA_VALUE = 5;
+    public static final int REDUCE_VALUE = MAX_VALUE / 2;
+
     private int processValue = 0;
     private IBinder binder = new ProcessBinder();
     private Lock lock = new ReentrantLock();
-
-    public interface UpdateUI {
-        public void updateUI(int value);
-    }
-
+    private ScheduledExecutorService executorService;
 
     public ProcessService() {
+
     }
+
+    Runnable processTask = () -> {
+       addProcessValue(DELTA_VALUE);
+        Log.d("ProcessService", "progress is " + processValue);
+    };
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -30,10 +40,16 @@ public class ProcessService extends Service {
 
     @Override
     public void onCreate() {
-
+        executorService = new ScheduledThreadPoolExecutor(1);
+        executorService.scheduleAtFixedRate(processTask, 200, 200, TimeUnit.MILLISECONDS);
     }
 
-    private class ProcessBinder extends Binder {
+    @Override
+    public void onDestroy() {
+        executorService.shutdownNow();
+    }
+
+    public class ProcessBinder extends Binder {
         ProcessService getProcessService() {
             return ProcessService.this;
         }
@@ -42,7 +58,7 @@ public class ProcessService extends Service {
     private void addProcessValue(int value) {
         synchronized (lock) {
             processValue += value;
-            if (processValue > 100) {
+            if (processValue > MAX_VALUE) {
                 processValue = 0;
             }
         }
@@ -50,8 +66,8 @@ public class ProcessService extends Service {
 
     private void reduceProcessValue() {
         synchronized (lock) {
-            if (processValue > 50) {
-                processValue -= 50;
+            if (processValue > REDUCE_VALUE) {
+                processValue -= REDUCE_VALUE;
             }
             else {
                 processValue = 0;
